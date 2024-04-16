493ae589c9edb23bd33c4197aa80ab474e219ff7
Slowikxx <aniakslowik@student.agh.edu.pl>
2024-04-16 18:32:16 +0200

    - created hero page - added neccessary images - installed animation and icon library

---
+++ public/index.ts
@@ -0,0 +1,17 @@
+import heroPhoto from './hero_photo.png';
+import darkBlob from './dark-blob.png';
+import lightBlob from './light-blob.png';
+import waves from './waves.png';
+import triangles from './triangles.png';
+import darkCircut from './dark-circut.png';
+import lightCircut from './light-circut.png';
+
+export {
+	heroPhoto,
+	darkBlob,
+	lightBlob,
+	waves,
+	triangles,
+	darkCircut,
+	lightCircut,
+};
